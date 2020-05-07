2af670f90446521147e5857e7f5d47736149db82
Annaelle Jaffrelot <annaellejaffreot@outlook.fr>
2020-05-07 17:21:04 +0200

    test db

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+var mysql = require('mysql');
+ 
+console.log('conneté');
+ 
+var conn = mysql.createConnection({
+  database: 'notes',
+  host: "localhost",
+  user: "root",
+  password: ""
+});
+ 
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
 
 'use strict';
 /*const {Article} = require ('./ models')*/
 const Hapi = require('@hapi/hapi');
+const Joi = require("joi");
+/*const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/test',
+ {useNewUrlParser: true, useUnifiedTopology: true});*/
 
 const init = async () => {
 
@@ -29,10 +49,34 @@ const init = async () => {
     server.route({
     method: "POST",
     path: "/note",
+   /* config:{
+    	validate:{
+    		payload:{
+    			name : Joi.string().required(),
+    			content : Joi.string().required(),
+    		}
+    	}
+    },*/
     handler: (request, response) => {
         return(request.payload);
     }
 });
+
+    server.route({
+    method: "DELETE",
+    path: "/note/{id}",
+    handler: (request, response) => {
+        var notes = {};
+        if(request.params.id == "1") {
+            notes = {
+                "id": "1",
+                "id_user": "1",
+                "name": "nom",
+                "content": "lorem"
+            }
+        }
+        return(notes);
+    }});
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
